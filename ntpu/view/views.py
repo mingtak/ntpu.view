5374cb3710bfb27df096f65e6722449cc87927ce
andy fang <andy@mingtak.com.tw>
2015-04-15 17:35:51 +0100

    backup

--- ntpu/view/views.py
+++ ntpu/view/views.py
@@ -5,9 +5,33 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 #from plone.app.contenttypes.interfaces import IDocument, INewsItem, IEvent
 #from plone.app.multilingual.interfaces import ILanguage
 
+from zope.component import getMultiAdapter
+
 
 grok.templatedir('views_template')
 
+
+class ToLanguageHome(grok.View):
+    grok.context(Interface)
+    grok.name('to_language_home')
+
+    def render(self):
+        context = self.context.aq_inner
+        response = self.response
+        portal = api.portal.get()
+        portal_state = getMultiAdapter((context, self.request), name=u'plone_portal_state')
+        current_language = portal_state.language()
+        url = '%s/system/index_%s' % (portal.absolute_url(), current_language)
+        response.redirect(url)
+        return
+
+
+class IsAnonymous(grok.View):
+    grok.context(Interface)
+    grok.name('isAnonymous')
+
+    def render(self):
+        return api.user.is_anonymous()
 
 class MyPage(grok.View):
     grok.context(Interface)
@@ -24,3 +48,19 @@ class MyPage(grok.View):
         else:
             response.redirect(context.absolute_url())
         return
+
+
+class CurrentLanguage(grok.View):
+    """
+    Get current language code
+    """
+
+    grok.context(Interface)
+    grok.name('currentLanguage')
+
+    def render(self):
+        context = self.context.aq_inner
+        portal_state = getMultiAdapter((context, self.request), name=u'plone_portal_state')
+        self.current_language = portal_state.language()
+
+        return self.current_language
